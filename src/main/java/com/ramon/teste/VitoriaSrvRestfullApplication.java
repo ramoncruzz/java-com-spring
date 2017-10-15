5020da2db8cc7cf43fabfb22265258411128731e
Ramon <ramon@parkcode.mobi>
2017-10-15 10:45:11 -0200

    primeiro commit

---
+++ src/main/java/com/ramon/teste/VitoriaSrvRestfullApplication.java
@@ -0,0 +1,12 @@
+package com.ramon.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitoriaSrvRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitoriaSrvRestfullApplication.class, args);
+	}
+}
