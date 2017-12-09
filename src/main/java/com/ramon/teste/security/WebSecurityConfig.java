8efaf23f81d83744345ba31e96516c9ff2c8ec49
Ramon <ramon@parkcode.mobi>
2017-12-08 23:43:37 -0200

    ajustes

--- src/main/java/com/ramon/teste/security/WebSecurityConfig.java
+++ src/main/java/com/ramon/teste/security/WebSecurityConfig.java
@@ -33,10 +33,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable().authorizeRequests()
 			.antMatchers("/").permitAll()
-			.antMatchers(HttpMethod.POST,"/usuario").permitAll()
-			.antMatchers(HttpMethod.GET,"/usuario").permitAll()
-			.antMatchers(HttpMethod.GET,"/autorizacao/popular").permitAll()
-			.antMatchers(HttpMethod.GET,"/pedidosMobile").hasAuthority("USER")
+//			.antMatchers(HttpMethod.POST,"/usuario").permitAll()
+//			.antMatchers(HttpMethod.GET,"/usuario").permitAll()
+//			.antMatchers(HttpMethod.GET,"/autorizacao/popular").permitAll()
+//			.antMatchers(HttpMethod.GET,"/pedidosMobile").hasAuthority("USER")
 			.anyRequest().authenticated()
 			.and()
 			// filtra requisições de login
