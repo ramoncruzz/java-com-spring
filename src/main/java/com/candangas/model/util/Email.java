82f80888351cdb75ba43f545f7b9e477d99ddbd8
Ramon <ramon@parkcode.mobi>
2018-01-21 12:16:03 -0200

    configuracoes banco dedados

--- src/main/java/com/candangas/model/util/Email.java
+++ src/main/java/com/candangas/model/util/Email.java
@@ -1,9 +1,10 @@
 package com.candangas.model.util;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Email {
 	
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
